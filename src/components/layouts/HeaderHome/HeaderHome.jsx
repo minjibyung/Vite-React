fd82009f10bc8cf7fe2243ed0542bad6b15f83ff
practica <minjibyung@gmail.com>
2023-02-17 17:57:29 -0500

    se agrega archivo

--- src/components/layouts/HeaderHome/HeaderHome.jsx
+++ src/components/layouts/HeaderHome/HeaderHome.jsx
@@ -1,4 +1,5 @@
 import NavBarHome from '../../ui/NavBarHome/NavBarHome.jsx'
+import ChangeColor from '../../pages/Home/ChangeColor.jsx'
 import React from 'react'
 
 export default function HeaderHome() {
